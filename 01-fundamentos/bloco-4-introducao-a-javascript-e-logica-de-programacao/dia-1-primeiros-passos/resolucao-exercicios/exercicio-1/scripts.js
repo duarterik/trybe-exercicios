a1bb767efea53d61263f7cacc703ef41f071bab4
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-08-26 12:09:21 -0300

    Exercicios com operadores aritmeticos

---
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/resolucao-exercicios/exercicio-1/scripts.js
@@ -0,0 +1,22 @@
+let a = 30;
+let b = 20;
+
+soma = a + b;
+
+console.log(soma);
+
+diminuicao = a - b;
+
+console.log(diminuicao);
+
+multiplica = a * b;
+
+console.log(multiplica);
+
+dividi = a / b;
+
+console.log(dividi);
+
+resto = a % b;
+
+console.log(resto);
