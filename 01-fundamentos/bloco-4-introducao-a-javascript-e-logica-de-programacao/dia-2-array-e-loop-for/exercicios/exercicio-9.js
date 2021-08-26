5ab7344b1f7bb950df59c6987d46f4736daa5b20
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-08-26 20:44:25 -0300

    Alterando informacoes no exercicio 9

--- 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios/exercicio-9.js
+++ 01-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicios/exercicio-9.js
@@ -1,9 +1,12 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 
+let valor = [];
 
-let valor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+for (let index = 1; index <= 25; index += 1) {
+    valor.push(index);
+}
 
-
+console.log(valor);
 for (let index = 0; index < valor.length; index += 1) {
     console.log(valor[index] / 2);
   };
